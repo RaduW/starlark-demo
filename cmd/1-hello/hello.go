4c1dffef86e447521231b49a9868b728080ee10b
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2022-05-12 13:18:05 +0200

    hello: embedded script

--- cmd/1-hello/hello.go
+++ cmd/1-hello/hello.go
@@ -5,25 +5,23 @@ import (
 	"go.starlark.net/starlark"
 )
 
-// !!! HERE !!!
-func CustomPrint(thread *starlark.Thread, msg string) {
-	fmt.Println("Here's what they want to print:\n>>>")
-	fmt.Print(msg)
-	fmt.Println("\n<<<")
-	// !!! END !!!
-}
+var script = `
+print("Embedded script")
+def add(a,b):
+	return a+b
+print("1 + 2 = ", add(1,2))
+`
 
 func main() {
 
 	// !!! HERE !!!
 	var thread = starlark.Thread{
-		Name:  "Starlark thread",
-		Print: CustomPrint,
+		Name: "Starlark thread",
 	}
 	// !!! END !!!
 
 	fmt.Println("\n Running starlark script \n")
-	var _, err = starlark.ExecFile(&thread, "./cmd/1-hello/hello.py", nil, nil)
+	var _, err = starlark.ExecFile(&thread, "Fancy-File.py", script, nil)
 	fmt.Println("\n Script finished \n")
 
 	if err != nil {
